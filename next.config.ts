914017cec41b5d822ddfdce181357da90871690b
vinicius <viniciuscicone@gmail.com>
2025-03-04 14:44:01 -0300

    changing next configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, 
+  },
+
+  images: {
+    domains: ["images.unsplash.com"],
+  },
 };
 
 export default nextConfig;
